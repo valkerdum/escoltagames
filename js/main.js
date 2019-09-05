2bcb3dbf7251165efaf853284f6877530921aa72
ofuentesp <ofuentesp@bancochile.cl>
2019-09-05 13:49:25 -0400

    se agregan configuraciones dl jugador

--- js/main.js
+++ js/main.js
@@ -33,6 +33,33 @@ $(".my-rating").starRating({
   useGradient: false
 });
 
+$(".my-rating-historial").starRating({
+  initialRating: 4,
+  totalStars: 5,
+  starSize: 20,
+  readOnly: true,
+  emptyColor: '#242424',
+  activeColor: 'white',
+  strokeWidth: 20,
+  strokeColor: 'white',
+  useGradient: false
+});
+
+$(".my-rating-carry").starRating({
+  initialRating: 0,
+  totalStars: 5,
+  starSize: 40,
+  readOnly: false,
+  emptyColor: '#3ae6c6',
+  activeColor: '#2A095F',
+  strokeWidth: 40,
+  strokeColor: '#2A095F',
+  useGradient: false,
+  hoverColor: '#2A095F',
+  ratedColor:	'#2A095F',
+  useFullStars: true
+});
+
 $(".rating-resenas-usuario").starRating({
   initialRating: 4,
   totalStars: 5,
